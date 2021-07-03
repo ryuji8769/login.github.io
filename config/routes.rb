c85665cba6ebf23e6a9801f10c4a530135e1d7ab
ruiji <abcdabcd0517@icloud.com>
2021-07-03 15:16:00 +0900

    ログアウト機能エラーなしで完成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   get 'login', to: "sessions#new"
   post 'login', to: "sessions#create"
-  get 'logout', to: "sessions#destroy"
+  delete 'logout', to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
