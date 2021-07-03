81a1a776699df40232e141d42cf1af3c6790b6ae
ruiji <abcdabcd0517@icloud.com>
2021-07-03 10:12:09 +0900

    新規登録完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+     user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path, notice:"登録完了"
+    else
+      render 'new'
+    end
+  end
+
   def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
