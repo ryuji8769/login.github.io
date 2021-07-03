3eeda3bad03dc1e313225eea2dc5ccd5df81a929
ruiji <abcdabcd0517@icloud.com>
2021-07-03 14:55:39 +0900

    ログイン、新規登録はエラーなしで完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
-class ApplicationController < ActionController::Base	
+class ApplicationController < ActionController::Base
+	helper_method :current_user
+
+	def login?
+		if current_user.nil?
+			redirect_to login_path, alert: "You have to login"
+		end
+	end
+
+	def already_login?
+		unless current_user.nil?
+			redirect_to user_path, notice: "You a already logged in"
+		end
+	end
+
+	def current_user
+		if session[:user_id]
+			current_user ||= User.find(session[:user_id])
+		end
+	end
 end
